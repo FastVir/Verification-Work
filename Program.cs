3c53df53d3cc33533783a9c52126bb3786ec24c1
Dimitry Timofeev <fasvir32gar@gmail.com>
2024-02-29 10:37:44 +0300

    Function to create an array FillArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] newArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return newArray;
+}
+
+
