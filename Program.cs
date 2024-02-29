3e43ebe638181111fc2f9c7b668fa7964fcdab00
Dimitry Timofeev <fasvir32gar@gmail.com>
2024-02-29 10:39:52 +0300

    function to print an array PrintArray

--- Program.cs
+++ Program.cs
@@ -8,4 +8,19 @@
     return newArray;
 }
 
+string PrintArray(string[] newArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (i == newArray.Length - 1)
+        {
+            stringArray += $"\"{newArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{newArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
 
